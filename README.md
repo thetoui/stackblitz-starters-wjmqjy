8741d65d8f5336827756712e8e01e2f481b3eac3
thetoui <mythetooi@gmail.com>
2024-09-03 14:23:37 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-wjmqjy
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/thetoui/stackblitz-starters-wjmqjy)
---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCvoif0MHSKKOdXVYSbbt9VUHKDmDaNnFs',
+  authDomain: 'optimistic-keel-269608.firebaseapp.com',
+  projectId: 'optimistic-keel-269608',
+  storageBucket: 'optimistic-keel-269608.appspot.com',
+  messagingSenderId: '847422876070',
+  appId: '1:847422876070:web:338163db5b5e9d57e14767',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
